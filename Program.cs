d7fbfe6572f0c63251e2f939240afe2649c14579
Fábio Cordeiro <fabio@pucminas.br>
2023-05-11 20:15:22 -0300

    Projeto Base

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Atividade Base!");
+
+Questao1.Executar();
---
+++ Questao1.cs
@@ -0,0 +1,7 @@
+class Questao1
+{
+    public static void Executar(){
+        Console.WriteLine("Atividade 1 -----------: ");
+    }
+    
+}
